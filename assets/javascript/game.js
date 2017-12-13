45ad7a6bf0a414c3ffe320cae2cafda064cc6967
mjsimon91 <mjsimon91@gmail.com>
2017-12-12 23:30:02 -0500

    Most HTML done and now adding psuedo code for javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+// Create an object for Yoda
+// Create an object for Luke
+// Create an object for Obi Wan
+
+//Create an object for Darth Vadar
+//Create an object for Kylo Ren
+//Create an object for Count
+
+//Create an on click function for good Guys
+  //Move selection to the goodCharacter div
+  //Hide the selected character from the good characters divs
+
+//Create an On click for the bad guys
+  //move selection to the badCharacter div
+  //Hide the selected character from the bad characters selection area
+
+//Create an on click for the attack button
+  //Power should be multiplied by 2 after each attack starting at 0
+  //Dedeuct health from the bad character
+    // if health is less then 0, then select next bad character
+      //Next bad character moves from original div to the battling bad character div
+    // if health is not below 0, counter attack
+      //Deduct health from the good character
+      //If good character health is below 0, then game over
